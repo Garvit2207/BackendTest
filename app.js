81d7fd141e1500ba541886c96d4df4b01eb1553c
Garvit2207 <91944978+Garvit2207@users.noreply.github.com>
2024-10-27 03:14:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,90 @@
+const express = require('express')
+require('./mongoose/mongoose')
+
+const Hostler = require('./models/hostlers')
+const ToClean = require('./models/CleaningRecord')
+const Cleaner = require('./models/cleaners')
+const cors = require('cors')
+
+const session = require('express-session')
+
+const app = express()
+app.use(cors())
+
+
+app.use(session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 30 * 60 * 1000 } // Session expires after 30 minutes
+}));
+
+
+const HostlerRouter = require('./routers/hostlers')
+const CleanerRouter = require('./routers/cleaners')
+
+app.use(express.json())
+app.use(HostlerRouter)
+app.use(CleanerRouter)
+
+// for Administrator pusrpose only
+
+app.post('/newhostler', async (req, res)=>{
+
+    console.log(req.body)
+    try{
+        const hostler = new Hostler(req.body)
+        await hostler.save()
+        res.status(200).send(hostler)
+    }
+    catch(e){
+        console.log(e)
+        res.send(e)
+    }
+})
+
+app.post('/newcleaner', async (req, res)=>{
+
+    console.log(req.body)
+
+    try{
+        const cleaner = new Cleaner(req.body)
+        await cleaner.save()
+        res.status(200).send(cleaner)
+    }
+    catch(e){
+        console.log(e)
+        res.send(e)
+    }
+
+})
+
+app.get('/', (req, res)=>{
+
+    res.send('hello')
+
+})
+
+app.get('/FetchTest', async (req, res)=>{
+
+    try{
+        const requests = await ToClean.find({})
+        console.log(requests)
+        res.send(requests)
+    }
+    
+    catch(e){
+    
+        console.log(e)
+        res.send(e)
+    
+    }
+    })    
+
+// Main Endpoints ---- use routers
+
+
+
+app.listen(3000, ()=>{
+    console.log('server is up!')
+})
